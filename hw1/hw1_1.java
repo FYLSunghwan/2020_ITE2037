cf362acaeb374ff7df97475833df7e83101671f5
FYLSunghwan <sunghwan519@hotmail.com>
2020-03-21 14:57:41 +0900

    hw: hw1 uploaded

---
+++ hw1/hw1_1.java
@@ -0,0 +1,17 @@
+public class hw1_1 {
+    public static void main(String[] args) {
+        int princi_amnt = 1000;
+        double int_rate = 5;
+        int no_of_yrs = 5;
+        double simple_int = princi_amnt * int_rate * no_of_yrs / 100;
+        System.out.println(simple_int);
+    }
+}
+
+
+
+
+
+
+
+
---
+++ hw1/hw1_2.java
@@ -0,0 +1,11 @@
+public class hw1_2 {
+    public static void main(String[] args) {
+        int total_sec = 50391;
+        int hour = total_sec/3600;
+        int min = (total_sec%3600)/60;
+        int sec = (total_sec%60);
+        System.out.println(hour+" hours, "+min+" minutes, and "+sec+" seconds.");
+    }
+}
+
+
